e3f7d15e12935f383c19a2ee0e248bb9e92a4120
Dhruv <dhruvsahu7355@gmail.com>
2025-07-22 22:48:38 +0530

    postForm components is added

--- src/components/index.js
+++ src/components/index.js
@@ -2,6 +2,6 @@ import Header from './header/Header';
 import Footer from './footer/Footer';
 import Container from './container/Container';
 import LogoutBtn from './header/LogoutBtn';
+import RTE from './RTE'
 
-
-export  { Header, Footer,Container,LogoutBtn };
+export  { Header, Footer,Container,LogoutBtn,RTE };
